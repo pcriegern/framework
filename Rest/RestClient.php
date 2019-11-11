241baad08dd27a4bd236417c52ed0c7abae884b8
wlec <57619620+wlec@users.noreply.github.com>
2019-11-11 12:43:38 +0100

    Namespace added

--- Rest/RestClient.php
+++ Rest/RestClient.php
@@ -1,24 +1,6 @@
 <?php
 
-/**
- *
- * White Label POS Sample Implementation (POS SDK)
- *
- * @author Philipp von Criegern <admin@the-white-label.com>
- * @copyright 2019 Philipp von Criegern, WHite Label Ecommerce GmbH
- * @license https://opensource.org/licenses/GPL-3.0 GNU Public License
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- */
+namespace wlec\Framework\Rest;
 
 /**
  * Class RestClient
