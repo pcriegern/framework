1dd12baa62122728c67dee6a94eb1fd86a02ddad
wlec <57619620+wlec@users.noreply.github.com>
2019-12-08 17:40:44 +0100

    Cache Reworked

--- Rest/ApiClient.php
+++ Rest/ApiClient.php
@@ -7,14 +7,26 @@ require_once __DIR__ . '/RestClient.php';
 class ApiClient extends RestClient {
 
     /**
-     * @var string
+     * Disable RestClient Cache
+     * @var int
      */
-    private $uriBase = '';
-
+    protected $cacheTTL     = 0;
     /**
      * @var string
      */
-    private $uri     = '';
+    protected $cacheDir     = '/tmp/';
+    /**
+     * @var int
+     */
+    protected $ttlTolerance = 3600;
+    /**
+     * @var string
+     */
+    private $uriBase        = '';
+    /**
+     * @var string
+     */
+    private $uri            = '';
 
     /**
      * Api constructor.
@@ -35,6 +47,7 @@ class ApiClient extends RestClient {
         }
         parent::__construct($url, $port);
         $this->setProtocol($protocol);
+        $this->addRequestHeader('Referer', $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
     }
 
     /**
@@ -46,17 +59,47 @@ class ApiClient extends RestClient {
      */
     public function handleRequest ( $uri, $isPostRequest = false, $postData = null, $requestMethod = 'GET' ) {
         $uri = preg_replace('/\/+/', '/', $this->uriBase . '/' . $uri);
-        return parent::handleRequest($uri, $isPostRequest, $postData, $requestMethod);
-    }
 
-    /**
-     * @param string $uri
-     * @param bool $useCache
-     * @return mixed
-     */
-    public function get ( $uri, $useCache = false ) {
-        $this->uri = '';
-        return parent::get($uri, false);
+        if ($requestMethod !== 'GET') {
+            // POST, PUT, DELETE, etc: No Cache
+            return parent::handleRequest($uri, $isPostRequest, $postData, $requestMethod);
+        }
+
+        $cacheFilename  = $this->cacheDir . 'api-' . md5($uri);
+        $cacheFileFound = false;
+        if (is_file($cacheFilename)) {
+            $cacheFileFound = true;
+            $cacheContent   = file_get_contents($cacheFilename);
+            $cacheEOL       = substr($cacheContent, 0, 10);
+            $cacheTTL       = $cacheEOL - time();
+            $json           = substr($cacheContent, 10);
+            $data           = json_decode($json, !$this->jsonDecodeObject);
+
+            // Cache is still valid
+            if ($cacheTTL > 0) {
+                return $data;
+            }
+
+            // Cache is outdated, but not too old: Refresh it (for concurrent requests)
+            if ($cacheTTL > -$this->ttlTolerance) {
+                touch($cacheFilename);
+            }
+        }
+
+        // Fetch Remote API Data
+        $data = parent::handleRequest($uri, $isPostRequest, $postData, $requestMethod);
+
+        // Extract Header and determine Cache TTL
+        if (preg_match('/Cache-Control: max-age=(\d+)/', $this->header, $var)) {
+            $cacheTTL = (int) $var[1];
+            $cacheEOL = time() + $cacheTTL;
+            file_put_contents($cacheFilename, $cacheEOL . $this->response);
+        } else if ($cacheFileFound) {
+            // No Cache, but File found: Delete it
+            unset($cacheFilename);
+        }
+
+        return $data;
     }
 
     /**
